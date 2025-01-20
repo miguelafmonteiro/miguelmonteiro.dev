baf4574d373554ce441ebbd4a93022c2ba31cd23
Miguel Monteiro <miguel.af.monteiro5@gmail.com>
2025-01-20 21:36:03 +0000

    Undo App.jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,15 @@ import { motion } from 'framer-motion';
 
 function App() {
   return (
-    // <div className="min-h-screen">
-    //   <Matrix className="absolute inset-0" />
-    //   <motion.div className="flex flex-col">
-    //     <Home />
-    //   </motion.div>
-    // </div>
-    // <>
-    //   <div className="fixed inset-0 w-full h-full bg-black z-0 pointer-events-none">
-    //     <Matrix className="absolute inset-0" />
-    //   </div>
+    <>
+      <div className="fixed inset-0 w-full h-full bg-black z-0 pointer-events-none">
+        <Matrix className="absolute inset-0" />
+      </div>
 
-    //   <motion.div className="relative z-10">
-    //     <Home />
-    //   </motion.div>
-    // </>
-    <div>
-      <Routes>
-        <Route path={`/miguelmonteiro.dev`} element={<Home />} />
-      </Routes>
-    </div>
+      <motion.div className="relative z-10">
+        <Home />
+      </motion.div>
+    </>
   );
 }
 
